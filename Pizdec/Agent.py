acb107acd150366d2fcbf6d43bc462a6c8bda952
9i5BCrUcNX5NmT <88964539+9i5BCrUcNX5NmT@users.noreply.github.com>
2021-08-24 14:16:50 +0300

    Update Agent.py

--- Pizdec/Agent.py
+++ Pizdec/Agent.py
@@ -16,21 +16,25 @@ class Agent(Holy_Grail):
 
     def Buy(self, how, price):
         # abc = requests.request(f'{self.url}/{self.id_}/new_lot_buy/{price}/{how}/')
-
+        how = how * 0.012
         url_ = f'{self.url}{self.id_}/new_lot_buy/{price}/{how}/'
         print(url_)
         abc = requests.request('GET', url=url_)
         print(abc)
-        # print('Покупка', how, 'По цене', price)
 
     def Sell(self, how, price):
+        how = how * 0.012
         url_ = f'{self.url}{self.id_}/new_lot_sell/{price}/{how}/'
         print(url_)
         abc = requests.request('GET', url=url_)
         print(abc)
-        # from Price import MinSellPrice
-        # Выставить энергию с ценой MinSellPrice(kargs)
-        # print('Продажа', how, 'По цене', price)
+    
+    def AAA(self, how, price):
+        how = how * 0.012
+        # Дать сигнал помощи
+        # from MaxBuyPrice import MaxBuyPrice
+        # Купить при цене MaxBuyPrice
+        pass
 
     def Graph(self, y_values, x_values, d, x_values1, y_values1, d1):
         # pow - заярд аккум
@@ -47,7 +51,7 @@ class Agent(Holy_Grail):
     # два массива (массив продажи и покупки ['продажа', проценты сколько надо, время начала промежутка графика, время конца промежутка графика, время промежутка)
     # (двумерный массив[цена])
 
-    def Check(self, time):
+    def Check(self, time, pow):
         k = 0
         k1 = 0
         for i in range(len(self.data['prices'])):
@@ -57,9 +61,11 @@ class Agent(Holy_Grail):
                 # print(self.data['graph'][k1][0])
                 if self.data['graph'][k1][4] <= time <= self.data['graph'][k1][6]:
                     if self.data['graph'][k1][0] == 'Покупка':
+                        if pow < 25:
+                            self.AAA(self.data['graph'][k1][1], self.data['prices'][i][j])
+                            break
                         self.Buy(self.data['graph'][k1][1], self.data['prices'][i][j])
                     else:
                         self.Sell(self.data['graph'][k1][1], self.data['prices'][i][j])
                 k1 += 1
             k += 1
-
